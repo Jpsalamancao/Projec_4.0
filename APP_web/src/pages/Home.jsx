d9c9574698e24ff6bec99be4fada70b9b920d34b
unknown <jpsalamancao@gmail.com>
2021-11-29 23:11:01 -0500

    se inicio el home del proyecto EN APP

--- APP_web/src/pages/Home.jsx
+++ APP_web/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Header from '@components/Header';
-import Columnas from '@components/Columns';
-import Multi_lineal from '@components/Multi_lineal';
+import Grafica_comparativas from '@containers/Grafica_comparativas';
+
 
 
 const Home = () => {
@@ -11,11 +11,7 @@ const Home = () => {
 	return (
 		<>
 			<Header />
-			<div>
-				<h1> CONTENIDO DEL HOME</h1>
-				<Columnas/> 
-			    <Multi_lineal/> 
-			</div>
+			<Grafica_comparativas/>
 
 		</>
 	);
