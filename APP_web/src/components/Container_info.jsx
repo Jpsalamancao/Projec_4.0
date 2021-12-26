ebdd32f9ffe9e8d93bb36a11b098e66ab01c61b2
unknown <jpsalamancao@gmail.com>
2021-12-26 11:04:48 -0500

    Se modifico el nombre de las graficas del Home

---
+++ APP_web/src/components/Container_info.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import "../styles/Container_info.scss";
+
+
+const Container_info = () => {
+    return (
+        <div className="Container_info">
+            <div className="item">
+                <h2>Responsable</h2>
+                <div className="box"></div>
+            </div>
+            <div className="item">
+                <h2>Lote</h2>
+                <div className="box"></div>
+            </div>
+            <div className="item">
+                <h2>Cantidad</h2>
+                <div className="box"></div>
+            </div>
+            <div className="item">
+                <h2>Rendimiento</h2>
+                <div className="box"></div>
+            </div>
+            <div className="item">
+                <h2>Varsol</h2>
+                <div className="box"></div>
+            </div>
+            <div className="item">
+                <h2>Alkonat</h2>
+                <div className="box"></div>
+            </div>
+            <div className="item">
+                <h2>Proquat BC-50</h2>
+                <div className="box"></div>
+            </div>
+            <div className="item">
+                <h2>Acido propinico</h2>
+                <div className="box"></div>
+            </div>
+            <div className="item">
+                <h2>Acido 2 Etil Hexoico</h2>
+                <div className="box"></div>
+            </div>
+            <div className="item">
+                <h2>Metal Cobalto en Polvo</h2>
+                <div className="box"></div>
+            </div>
+            <div className="item">
+                <h2>Tierra Filtratadicamex 4170</h2>
+                <div className="box"></div>
+            </div>
+
+        </div>
+    );
+
+}
+
+export default Container_info;
