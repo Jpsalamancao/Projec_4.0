4a108b373394c4894e554663002533b1665d51e1
KrlozMedina <camedinal@correo.udistrital.edu.co>
2022-01-27 23:10:13 -0500

    Probando webpack

--- webpack.config.js
+++ webpack.config.js
@@ -59,7 +59,10 @@ module.exports = {
   ],
   devServer: {
     historyApiFallback: true,
-    contentBase: path.join(__dirname, 'dist'),
+    // contentBase: path.join(__dirname, 'dist'),
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
     compress: true,
     port: 3006
   }  
