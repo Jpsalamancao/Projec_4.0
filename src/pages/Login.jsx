c843a5cb58034ecf11a888190bb1d35624e80b32
KrlozMedina <camedinal@correo.udistrital.edu.co>
2022-02-06 18:48:14 -0500

    Modificando Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,6 @@
 import React, {useRef} from "react";
 import '@styles/Login.scss';
-import logo from '@Logos/PQ_logo.png'
-
-    
+import logo from '@Logos/UD.png'
 
 const Login = () => {
 	const form = useRef(null);
@@ -11,45 +9,45 @@ const Login = () => {
 		event.preventDefault();
 		const formData = new FormData(form.current);
 		const data = {
-			usename: formData.get('email'),
+			username: formData.get('email'),
 			password: formData.get('password')
 		}
 		console.log(data);
     }
+
     return (
         <div className="container_PQ">
-            <div className="semicirculo">
-            </div>
+            <div className="semicirculo"></div>
+
             <div className="container">
                 <div className="container_logo">
                     <img src={logo}
-                    className="Producciones-Quimicas-logo"
+                    className="UniversidadDistritalLogo"
                     alt="logo"/>
                 </div>
+
                 <form action="/" className= "form" ref={form}>
                     <div className="container_data">
                         <p>Usuario</p>
-                        <input type="text" name="email" className="input" 
-                        placeholder="jpsalaman"/>
+                        <input type="text" name="email"
+                        className="input" placeholder="user@email"/>
+
                         <p>Contraseña</p>
-                        <input type="password" name="password" className="input"
-                        placeholder="12345"/>
+                        <input type="password" name="password"
+                        className="input" placeholder="********"/>
                     </div>
 
-                    <button className="boton" href="/Home" 
-                    onClick ={ handleSubmit}>
+                    <button className="boton" href="/Home" onClick ={handleSubmit}>
                         Ingresar               
                     </button>
                 </form>
 
                 <div className="container_link">
-                    <p><a href="/">¿Olvido la contraseña?</a></p>
+                    <p><a href="/Home">¿Olvido la contraseña?</a></p>
                     <p><a href="/">Crear usuario</a></p>
                 </div>
             </div>
-
         </div>
-
     );
 }
 
